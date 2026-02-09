5390cddcc47e0a714758c01ca1ba811cfc2d3157
iamsai2003 <vadlasaikumar12@gmail.com>
2026-02-09 10:31:54 +0530

    Auto-create superuser

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin123') if not User.objects.filter(username='admin').exists() else print('Superuser already exists')"
